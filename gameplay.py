7a420f0e254f63a165a9f58320ff93d249c95f8d
Etay Schwartzberg <etay.schwartzberg@post.idc.ac.il>
2022-09-06 18:13:53 +0300

    Seperate the sketch from hangman to gameplay

--- gameplay.py
+++ gameplay.py
@@ -1,13 +1,26 @@
-from mimetypes import init
 from hangman import Hangman
 from sketch import Sketch
+import consant_sketches as cs
 
 class Gameplay():
     def __init__(self) -> None:
         self._hangman = Hangman()
         self._sketch = Sketch()
-        self._sketch.sketch = Sketch.hanged_man()    
+        self._sketch.sketch = Sketch.hanged_man()
 
+    def show_sketch(self):
+        return '\n'.join(''.join(line) for line in self._sketch.sketch)
+
+    def lose_part(self, part):
+        lost_parts = self._hangman.lose_part(part)
+        for lost_part in lost_parts:
+            y_coord, x_coord, _ = cs.HANGMAN[part.kind]
+            # TODO: Generalize according to Sketch specs
+            self._sketch.sketch[y_coord + 2][x_coord + 10] = ' '
+
+    def drop_part(self, part, x_coord, y_coord):
+        self._sketch.sketch[y_coord + 1][x_coord] = self._sketch.sketch[y_coord][x_coord]
+        self._sketch.sketch[y_coord][x_coord] = ' '
 
 if __name__ == '__main__':
     game = Gameplay()
--- hangman.py
+++ hangman.py
@@ -1,7 +1,6 @@
 import heapq
 from body_parts import BodyParts as bp
 from part import Part
-from sketch import Sketch
 
 import os
 from time import sleep
@@ -12,51 +11,55 @@ class Hangman:
         self._hangman_body = [(part.rank, part) for part in self._parts.values()]
         heapq.heapify(self._hangman_body)
 
-    def show_sketch(self):
-        return '\n'.join(''.join(line) for line in self._sketch)
-
-    def lose_part(self, part=None):
+    def lose_part(self, part : bp = None):
         lost_parts = []
         if part:
+            # TODO: Fix for missing hand
             if part == bp.LOWER_BODY:
                 lower_body_rank = self._parts[bp.LOWER_BODY].rank
-                hands_order = int(self._parts[bp.LEFT_HAND].rank > self._parts[bp.RIGHT_HAND].rank)
+                hands_order = int(
+                    self._parts[bp.LEFT_HAND].rank > self._parts[bp.RIGHT_HAND].rank
+                    )
                 self._parts[bp.LEFT_HAND].rank = lower_body_rank + 1 + hands_order
                 self._parts[bp.RIGHT_HAND].rank = lower_body_rank + 2 - hands_order
 
-            while(min(self._hangman_body)[-1].kind != part):
-                _, dropped_part = heapq.heappop(self._hangman_body)
-                self.detach_part(dropped_part)
-        _,  lost_part = heapq.heappop(self._hangman_body)
-        lost_parts.append(lost_part)
-        lost_part.detach()
+            if self._parts[part].kind.is_core:
+                while min(self._hangman_body)[-1].kind != part:
+                    self._detach_part(lost_parts)
+            else:
+                # TODO: Look for a better way of removing a non-core part
+                hold_parts = []
+                while min(self._hangman_body)[-1].kind != part:
+                    hold_parts.append(heapq.heappop(self._hangman_body))
+                self._detach_part(lost_parts)
+                while hold_parts:
+                    heapq.heappush(self._hangman_body, hold_parts.pop())
+        else:
+            self._detach_part(lost_parts)
+
         return lost_parts
 
-    def detach_part(self, part: Part):
-        part.detach()
-        y, x, _ = Sketch.PERSON[part.kind]
-        self._sketch[y+2][x+10] = ' ' # TODO: Generalize according to Sketch specs
-
-    def drop_part(self, part, x, y):
-        self._sketch[y + 1][x] = self._sketch[y][x]
-        self._sketch[y][x] = ' '
-        
+    def _detach_part(self, lost_parts: list[Part]):
+        _, lost_part = heapq.heappop(self._hangman_body)
+        lost_part.detach()
+        lost_parts.append(lost_part)
 
 # ----------------------------------------------------------------------
 def show():
-    os.system('clear') if os.name == 'posix' else os.system('cls')
-    print(hm.show_hangman())
+    _ = os.system('clear') if os.name == 'posix' else os.system('cls')
+    #print(hm.show_hangman())
     sleep(1)
 
 def step(part=None):
-    os.system('clear') if os.name == 'posix' else os.system('cls')
+    _ = os.system('clear') if os.name == 'posix' else os.system('cls')
     hm.lose_part(part)
-    print(hm.show_hangman())
+    #print(hm.show_hangman())
     sleep(1)
 
 if __name__ == '__main__':
     hm = Hangman()
-    show()
+    print([p.kind for p in hm.lose_part(bp.LEFT_LEG)])
+    #show()
     # for _ in range(7):
     #     step()
     # step(bp.HEAD)
