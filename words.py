7dfde22a5853e3baeb2689ec047b6baaabe8c3fb
Etay Schwartzberg <etay.schwartzberg@post.idc.ac.il>
2022-09-06 18:22:53 +0300

    Removed API key until hiding fix (previous key is not active)

--- words.py
+++ words.py
@@ -4,11 +4,13 @@ class Words:
     def __init__(self):
         self._url = "https://wordsapiv1.p.rapidapi.com/words/"
         self._headers = {
-            "X-RapidAPI-Key": "e452d91c62msh135bbc5190b59c6p1ba3c6jsnc53b4dc602f7",
+            # TODO: Setup .env file to hold API key
+            # FIXME: Insert the new API KEY
+            "X-RapidAPI-Key": "",
             "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
             }
         self._words = []
-    
+
     def random(self, length=None) -> str:
         payload = {'random': 'true'}
         if length:
@@ -16,14 +18,14 @@ class Words:
         response = requests.get(f'{w._url}', headers=w._headers, params=payload)
         if response.status_code == 200:
             return response.json()['word']
-        
+
 
 if __name__ == '__main__':
     w = Words()
     # payload = {'random': 'true', 'letters': '4'}
     # response = requests.get(f'{w._url}', headers=w._headers, params=payload)
     # if response.status_code == 200:
-    #     word = response.json()['word'] 
+    #     word = response.json()['word']
     word = w.random(5)
     print(word)
     mistakes = []
